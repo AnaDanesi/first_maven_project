c9c2e6c30288fa08bb5ba86e68425c4e0fd989cd
Anabella Danesi <anetadanesi@gmail.com>
2023-03-17 20:13:22 +0100

    css selectors day 19

--- src/main/java/day19/CSSSelectorsDemo.java
+++ src/main/java/day19/CSSSelectorsDemo.java
@@ -46,7 +46,22 @@ public class CSSSelectorsDemo {
         driver.get("https://demo.nopcommerce.com/");
         driver.manage().window().maximize();
 
-        driver.findElement(By.cssSelector("input#small-searchterms")).sendKeys("MacBook");
+        //tag and id
+        //driver.findElement(By.cssSelector("input#small-searchterms")).sendKeys("MacBook");
+        //driver.findElement(By.cssSelector("#small-searchterms")).sendKeys("MacBook");
+
+        //tag and class
+        //driver.findElement(By.cssSelector("input.search-box-text")).sendKeys("MacBook");
+        //driver.findElement(By.cssSelector(".search-box-text")).sendKeys("MacBook");
+
+        //tag and attribute
+        //driver.findElement(By.cssSelector("input[name='q']")).sendKeys("MacBook");
+        //driver.findElement(By.cssSelector("[name='q']")).sendKeys("MacBook");
+
+        //tag class and attribute
+        //driver.findElement(By.cssSelector("input.search-box-text[name='q']")).sendKeys("MacBook");
+        driver.findElement(By.cssSelector("input.search-box-text[name='q']")).sendKeys("MacBook");
+
 
     }
 
