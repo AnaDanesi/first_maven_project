56ad7676c8e8718e4622b3ca8bb7755b7e95789c
Anabella Danesi <anetadanesi@gmail.com>
2023-03-20 17:59:31 +0100

    preceding and following siblings nodes

--- src/main/java/day21YouTube/XPathAxesDemo.java
+++ src/main/java/day21YouTube/XPathAxesDemo.java
@@ -18,23 +18,44 @@ public class XPathAxesDemo {
         driver.get("https://money.rediff.com/gainers/bse/daily/groupa");
         driver.manage().window().maximize();
 
-        //self
+        //Self  - Selects the current node
         String text=driver.findElement(By.xpath("//a[normalize-space()='Data Patterns (India']/self::a")).getText();
         System.out.println(text); //Data Patterns (India
 
-        //parent
+        //Parent - Selects the parent of the current node (always One)
         text= driver.findElement(By.xpath("//a[normalize-space()='Data Patterns (India']/parent::td")).getText();
         System.out.println(text); //Data Patterns (India
 
-        //child (actually our self node doesnt have a child, so we use the ancestor to go down
+        //Child - Selects all children of the current node (One or many) (actually our self node doesnt have a child, so we use the ancestor to go down
         List<WebElement> childs=driver.findElements(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr/child::td"));
         System.out.println("Number of child element:  "+ childs.size()); //Number of child element : 5
 
-        //ancestor
+        //Ancestor - Selects all ancestors (parent, grandparent, etc.)
         text=driver.findElement(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr")).getText();
         System.out.println(text); //Data Patterns (India A 1,350.20 1,417.00 + 4.95 (it brings all the data of the row because it doesnt have any text
 
+        //Descendant - Selects all descendants (children, grandchildren, etc.) of the current node (child and grandchild if we put * 7 elements, if we put td 5 elements)
+        List<WebElement> descendants=driver.findElements(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr/descendant::*"));
+        System.out.println("Number of descendants:  "+descendants.size()); //7 elements
 
+        //Following -Selects everything in the document after the closing tag of the current node
+        List<WebElement>followingnodes=driver.findElements(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr/following::tr"));
+        System.out.println("Number of following nodes:"+followingnodes.size()); //151 elements
+
+        //Following-sibling : Selects all siblings after the current node
+        List<WebElement> followingsiblings=driver.findElements(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr/following-sibling::tr"));
+        System.out.println("Number of Following Siblings:"+followingsiblings.size()); //149 elements
+
+        //Preceding - Selects all nodes that appear before the current node in the document
+        List<WebElement> precedings=driver.findElements(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr/preceding::tr"));
+        System.out.println("Number of preceding nodes:"+precedings.size()); //6 elements
+
+        //preceding-sibling - Selects all siblings before the current node
+        List<WebElement> precedingsiblings=driver.findElements(By.xpath("//a[normalize-space()='Data Patterns (India']/ancestor::tr/preceding-sibling::tr"));
+        System.out.println("Number of preceding sibling nodes:"+precedingsiblings.size()); //5 elements
+
+
+        driver.close();
 
     }
 }
