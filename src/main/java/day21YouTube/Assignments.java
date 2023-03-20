310cee9990651ca8752d723b002e1567c93bc255
Anabella Danesi <anetadanesi@gmail.com>
2023-03-20 18:16:57 +0100

    assignments

---
+++ src/main/java/day21YouTube/Assignments.java
@@ -0,0 +1,17 @@
+package day21YouTube;
+
+/*
+1)Find the sign-up button from the registration form in the facebook application (child)
+//div[@id='reg_form_box']/child::div[11]/button
+
+2)Locate Firstname from sign-up button in the same  page (parent)
+
+
+ */
+public class Assignments {
+
+    public static void main(String[] args) {
+
+
+    }
+}
