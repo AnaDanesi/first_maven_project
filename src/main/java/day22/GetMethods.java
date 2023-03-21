c837aac4b002642f4a421a101a532a78bcb72cf9
Anabella Danesi <anetadanesi@gmail.com>
2023-03-21 14:39:17 +0100

    get handles

--- src/main/java/day22/GetMethods.java
+++ src/main/java/day22/GetMethods.java
@@ -17,8 +17,11 @@ getWindowHandle()
 getwindowHandles()
  */
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
 
 public class GetMethods {
 
@@ -34,11 +37,19 @@ public class GetMethods {
 
         System.out.println("current url:   "+driver.getCurrentUrl());
 
-        System.out.println("Page source:   ");
+       /* System.out.println("Page source:   ");
         String ps=driver.getPageSource();
         System.out.println(ps);
-        System.out.println(ps.contains("html"));
+        System.out.println(ps.contains("html"));*/
 
+        //System.out.println(driver.getWindowHandle()); //browser id not constant
 
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("OrangeHRM, Inc")).click(); //open new browser window
+        Set<String> windowids=driver.getWindowHandles();
+
+        for(String winid:windowids){
+            System.out.println(winid);
+        }
     }
 }
