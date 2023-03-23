87bd6ac8a6c5bd92ba99e0908b4c0eb5f6bcada4
Anabella Danesi <anetadanesi@gmail.com>
2023-03-23 15:53:30 +0100

    implicit wait

---
+++ src/main/java/day23/ImplicitWait.java
@@ -0,0 +1,40 @@
+package day23;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+/*implicit wait
+----------
+Adv:
+1) single time/one statement
+2) it will not wait till maximum time if the element is availble
+3) Applicable for all the elements
+4) easy to use
+
+Disadvantge:
+1) if the time is not suffitient then you will get exception
+
+
+ */
+public class ImplicitWait {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //implicit command
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+
+    }
+}
