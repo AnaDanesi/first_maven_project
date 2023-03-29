1dc4f8002fc0df85e514c067f0e8752b44e8f312
Anabella Danesi <anetadanesi@gmail.com>
2023-03-29 20:24:11 +0200

    autosuggest dropdown

---
+++ src/main/java/day25/AutoSuggestDropdown.java
@@ -0,0 +1,38 @@
+package day25;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AutoSuggestDropdown {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium");
+        Thread.sleep(3000);
+
+        List<WebElement> list=driver.findElements(By.xpath("//li[@class='sbct']"));
+        System.out.println("Number of suggestions:  "+list.size());
+
+    }
+
+}
