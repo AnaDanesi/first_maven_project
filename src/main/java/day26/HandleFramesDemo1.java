1c3543b052966142bfc44a11b19885425cfd1843
Anabella Danesi <anetadanesi@gmail.com>
2023-04-03 18:51:11 +0200

    Handling frames

---
+++ src/main/java/day26/HandleFramesDemo1.java
@@ -0,0 +1,47 @@
+package day26;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+/*
+packageListFrame
+packageFrame
+classFrame
+
+ */
+public class HandleFramesDemo1 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html");
+        driver.manage().window().maximize();
+
+        driver.switchTo().frame("packageListFrame");
+        driver.findElement(By.linkText("org.openqa.selenium")).click(); //frame1
+
+        driver.switchTo().defaultContent(); //switch back to main page
+
+        driver.switchTo().frame("packageFrame");
+        driver.findElement(By.linkText("WebDriver")).click(); //frame2
+
+        driver.switchTo().defaultContent();
+
+        driver.switchTo().frame("classFrame");
+        driver.findElement(By.xpath("This element is in iframe - //a[normalize-space()='org.openqa.selenium']")).click();  //frame3
+
+
+
+
+    }
+}
