805a64817b3f8d05f7972a142319ec6652b3aec7
Anabella Danesi <anetadanesi@gmail.com>
2023-04-10 09:34:43 +0200

    assignment 2, swiching iframes

--- src/main/java/day26/Assignment2.java
+++ src/main/java/day26/Assignment2.java
@@ -6,9 +6,36 @@ iframe: put some text, click bold, click clear, put new text
 
  */
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
 public class Assignment2 {
 
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://the-internet.herokuapp.com/iframe");
+        driver.manage().window().maximize();
+
+        driver.switchTo().frame("mce_0_ifr");
+        driver.findElement(By.xpath("//p[normalize-space()='Your content goes here.']")).sendKeys("Hola Pavan");
+
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//button[@title='Bold']")).click();
+
+        driver.switchTo().frame("mce_0_ifr");
+        driver.findElement(By.cssSelector("body p")).clear();
 
 
     }
