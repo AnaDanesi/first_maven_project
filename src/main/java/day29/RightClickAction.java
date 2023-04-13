eafbce7ff7dc8c2bbb4897abab9feb310294f909
Anabella Danesi <anetadanesi@gmail.com>
2023-04-13 17:06:30 +0200

    right click action

--- src/main/java/day29/RightClickAction.java
+++ src/main/java/day29/RightClickAction.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
 public class RightClickAction {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -24,6 +25,17 @@ public class RightClickAction {
         driver.manage().window().maximize();
 
         WebElement button=driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+
+        Actions act=new Actions(driver);
+
+        //Right Click
+        act.contextClick(button).perform();
+
+        driver.findElement(By.xpath("//span[normalize-space()='Copy']")).click();  //click on copy option
+
+        Thread.sleep(5000);  //to see the alert window only
+
+        driver.switchTo().alert().accept(); //close the alert window
 
 
 
