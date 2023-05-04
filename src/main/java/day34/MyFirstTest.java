527e90bd77007dca3cb4efdad61510b714d71468
Anabella Danesi <anetadanesi@gmail.com>
2023-05-04 10:21:33 +0200

    Installing TestNG, dependencies, etc

---
+++ src/main/java/day34/MyFirstTest.java
@@ -0,0 +1,26 @@
+package day34;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test()
+
+public class MyFirstTest {
+
+    @Test
+    void test1() {
+        System.out.println("testing 1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("testing 2");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("testing 3");
+    }
+
+}
