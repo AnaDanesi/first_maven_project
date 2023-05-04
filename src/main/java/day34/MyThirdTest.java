093e13b88d81534a90044efc3b7d9687dba28a2f
Anabella Danesi <anetadanesi@gmail.com>
2023-05-04 11:10:03 +0200

    Testing reports

---
+++ src/main/java/day34/MyThirdTest.java
@@ -0,0 +1,21 @@
+package day34;
+
+import org.testng.annotations.Test;
+
+public class MyThirdTest {
+
+    @Test(priority = 1)
+    void test1() {
+        System.out.println("testing 1");
+    }
+
+    @Test(priority = 2)
+    void test2() {
+        System.out.println("testing 2");
+    }
+
+    @Test(priority = 3)
+    void test3() {
+        System.out.println("testing 3");
+    }
+}
