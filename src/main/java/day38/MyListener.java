25e3ca701a86896430a731e82976abd32255d84a
Anabella Danesi <anetadanesi@gmail.com>
2023-05-16 20:26:38 +0200

    TestNG Listeners

---
+++ src/main/java/day38/MyListener.java
@@ -0,0 +1,51 @@
+package day38;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+/*
+Day-38
+------------
+TestNG Listeners - look at the documentation
+-------------
+ITestListener ---> Interface
+TestListenerAdapter  -- Class
+
+onTestStart()
+onTestFailure()
+onTestSuccess()
+onTestSkipped()
+--
+--
+ */
+public class MyListener implements ITestListener { //or extends TestListenerAdapter
+
+
+    public void onTestStart(ITestResult result)
+    {
+        System.out.println("on test start....");
+    }
+
+    public void onTestSuccess(ITestResult result)
+    {
+        System.out.println("on test success.........");
+    }
+
+    public void onTestFailure(ITestResult result)
+    {
+        System.out.println("on test failure.........");
+    }
+
+    public void onTestSkipped(ITestResult result)
+    {
+        System.out.println("on test skipped.........");
+    }
+
+    public void onFinish(ITestContext context)
+    {
+        System.out.println("on test finish.........");
+    }
+
+
+}
